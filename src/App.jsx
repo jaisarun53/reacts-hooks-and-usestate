a752eb761f9ed26bdfa54a90fea58c87b9969e17
jaisarun53 <jaisarun53@gmail.com>
2024-04-25 19:44:33 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+
+const App = () => {
+  const [count, setcount] = useState(0);
+  console.log("count", count);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          console.log("Hi");
+          const newCount = count + 1;
+          setcount(newCount);
+        }}
+      >
+        Click me
+      </button>
+    </div>
+  );
+};
+
+export default App;
