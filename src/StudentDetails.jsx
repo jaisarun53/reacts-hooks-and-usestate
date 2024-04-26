a77af65d6bc343f804bb6a88fc0c53cc36c3a0f5
jaisarun53 <jaisarun53@gmail.com>
2024-04-26 22:01:49 +0545

    change

--- src/StudentDetails.jsx
+++ src/StudentDetails.jsx
@@ -11,13 +11,28 @@ const StudentDetails = () => {
       <h1>{studentinfo.name}</h1>
       <h3>Age:{studentinfo.age}</h3>
       <p>Enrolled collage:{studentinfo.collage}</p>
-      <button
-        onClick={() => {
-          setStudentinfo({ name: "Arun", age: 21, collage: "Aroma" });
-        }}
-      >
-        Change me
-      </button>
+      {/* name field */}
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(event) => {
+            const newName = event.target.value;
+            setStudentinfo({ ...studentinfo, name: newName });
+          }}
+        />
+      </div>
+      {/* age Field */}
+      <div>
+        <input
+          type="number"
+          placeholder="Age"
+          onChange={(event) => {
+            const newAge = event.target.value;
+            setStudentinfo({ ...studentinfo, age: newAge });
+          }}
+        />
+      </div>
     </div>
   );
 };
